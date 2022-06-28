dc65ba112a1bf9bbe5c131bcef5eec9ea436c647
Mortenso Kodehode <mortenso.kodehode@gmail.com>
2022-06-28 12:18:56 +0200

    Updates

---
+++ precache-manifest.79a9a50f87806dcd2ff23199227f7cd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7294869e10dfd5a24e80",
+    "url": "/react-portfolio/static/css/main.4ec89393.chunk.css"
+  },
+  {
+    "revision": "7294869e10dfd5a24e80",
+    "url": "/react-portfolio/static/js/main.7294869e.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/react-portfolio/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "d480420a20c8c51e3773",
+    "url": "/react-portfolio/static/js/2.d480420a.chunk.js"
+  },
+  {
+    "revision": "d50934acc4004fdc9fb9",
+    "url": "/react-portfolio/static/js/runtime~main.d50934ac.js"
+  },
+  {
+    "revision": "1d604584cc090c243c65f4a00a5d5534",
+    "url": "/react-portfolio/static/media/logo2.1d604584.svg"
+  },
+  {
+    "revision": "6d6ae1a0f02b84a5411a90d07f123ba3",
+    "url": "/react-portfolio/static/media/icons8-menu-75.6d6ae1a0.svg"
+  },
+  {
+    "revision": "81c62f99b7e4d92f13497d34316d75e5",
+    "url": "/react-portfolio/index.html"
+  }
+];
